e0aca180b4634e5063c1c714893a4574d0b97701
rpineda@illuminateed.net <rpineda@illuminateed.net>
2020-11-04 00:10:49 -0600

    GatewayApp running

---
+++ poc-api-gateway/srv/gateway/src/main/java/portaltek/pagw/gateway/GatewayApp.java
@@ -0,0 +1,21 @@
+package portaltek.pagw.gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+@RestController
+class GatewayApp {
+  @GetMapping("/rest/open/hi")
+  public String hi() {
+    return "hi!";
+  }
+
+  public static void main(String[] args) {
+    run(GatewayApp.class, args);
+  }
+}
+
