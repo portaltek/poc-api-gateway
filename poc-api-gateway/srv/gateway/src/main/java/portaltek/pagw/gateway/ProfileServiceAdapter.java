b42d4ed11f2d3a6cc4decc10d706d7811adc136c
rpineda@illuminateed.net <rpineda@illuminateed.net>
2020-11-04 23:40:38 -0600

    temp move web security

--- poc-api-gateway/srv/gateway/src/main/java/portaltek/pagw/gateway/ProfileServiceAdapter.java
+++ poc-api-gateway/srv/gateway/src/main/java/portaltek/pagw/gateway/ProfileServiceAdapter.java
@@ -3,13 +3,12 @@ package portaltek.pagw.gateway;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 import portaltek.pagw.common.web.user.GenericUserService;
 
 
 class ProfileServiceAdapter implements UserDetailsService {
 
-   static final String NOT_FOUND = "No user found with username '%s'.";
+   private static final String NOT_FOUND = "No user found with username '%s'.";
 
    private UserDetailsService userService;
 
