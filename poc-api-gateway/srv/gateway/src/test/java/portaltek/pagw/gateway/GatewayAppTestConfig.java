f520dfec70bf06368dfef9ee9057068d06fc7b82
rpineda@illuminateed.net <rpineda@illuminateed.net>
2020-11-06 20:21:44 -0600

    Adding AppProfile

--- poc-api-gateway/srv/gateway/src/test/java/portaltek/pagw/gateway/GatewayAppTestConfig.java
+++ poc-api-gateway/srv/gateway/src/test/java/portaltek/pagw/gateway/GatewayAppTestConfig.java
@@ -1,22 +1,13 @@
 package portaltek.pagw.gateway;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.test.context.ActiveProfiles;
-import portaltek.pagw.common.env.AppProfile;
-import portaltek.pagw.common.web.security.jwt.JwtUserFactory;
 import portaltek.pagw.common.web.test.Api;
 import portaltek.pagw.common.web.test.Header;
 import portaltek.pagw.common.web.test.Rest;
-import portaltek.pagw.gateway.spi.profile.LocalProfileServiceAdapter;
-
-import static portaltek.pagw.common.env.AppProfile.LOCAL;
 
 @Lazy
 @TestConfiguration
