b22a9cb409970d775ccde16fb6667d539f05f6cb
rpineda@illuminateed.net <rpineda@illuminateed.net>
2020-11-21 21:52:10 -0600

    Revert "Refactor unit test into groovy"
    This reverts commit 1a87b449f3f6d9a5e35433a7a911338b3361b8cf.

---
+++ poc-api-gateway/srv/gateway/src/test/java/portaltek/pagw/gateway/TestCredentials.java
@@ -0,0 +1,9 @@
+package portaltek.pagw.gateway;
+
+import portaltek.pagw.common.web.Credentials;
+
+public class TestCredentials {
+   public static final Credentials ADMIN = new Credentials("admin", "admin");
+   public static final Credentials ADMIN_INVALID = new Credentials("admin", "admin2");
+   public static final Credentials USER = new Credentials("user", "user");
+}
---
+++ poc-api-gateway/srv/gateway/src/test/java/portaltek/pagw/gateway/api/rest/GreetingControllerITest.java
@@ -0,0 +1,71 @@
+package portaltek.pagw.gateway.api.rest;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+import portaltek.pagw.common.web.ServerResponse;
+import portaltek.pagw.gateway.GatewayAppIntegrationTest;
+
+import static java.util.Objects.requireNonNull;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.OK;
+import static portaltek.pagw.gateway.TestCredentials.ADMIN;
+import static portaltek.pagw.gateway.TestCredentials.USER;
+
+
+class GreetingControllerITest extends GatewayAppIntegrationTest {
+
+   final String GET_ADMIN = "/api/auth/admin";
+   final String GET_USER = "/api/auth/user";
+   final String MSG_ADMIN = "Hello Authenticated Admin";
+   final String MSG_USER = "Hello Authenticated User";
+
+   @BeforeEach
+   void init() {
+   }
+
+   @Test
+   void givenAdminCredentials_getAdmin_shouldReturnMsg() {
+      var req = jwt.createReq(ADMIN, "");
+
+      ResponseEntity<ServerResponse> reply = api.get(GET_ADMIN, req, ServerResponse.class);
+
+      then(reply.getStatusCode()).isEqualTo(OK);
+      then(requireNonNull(reply.getBody()).getMessage()).isEqualTo(MSG_ADMIN);
+   }
+
+   @Test
+   void givenUserCredentials_getAdmin_shouldReturn403() {
+      var req = jwt.createReq(USER, "");
+
+      ResponseEntity<ServerResponse> reply = api.get(GET_ADMIN, req, ServerResponse.class);
+
+      then(reply.getStatusCode()).isEqualTo(FORBIDDEN);
+   }
+
+   @Test
+   void givenAdminCredentials_getUser_shouldReturnMsg() {
+      var req = jwt.createReq(ADMIN, "");
+
+      ResponseEntity<ServerResponse> reply = api.get(GET_USER, req, ServerResponse.class);
+
+      then(reply.getStatusCode()).isEqualTo(OK);
+      then(requireNonNull(reply.getBody()).getMessage()).isEqualTo(MSG_USER);
+   }
+
+   @Test
+   void givenUserCredentials_getUser_shouldReturnMsg() {
+      var req = jwt.createReq(USER, "");
+
+      ResponseEntity<ServerResponse> reply = api.get(GET_USER, req, ServerResponse.class);
+
+      then(reply.getStatusCode()).isEqualTo(OK);
+      then(requireNonNull(reply.getBody()).getMessage()).isEqualTo(MSG_USER);
+   }
+
+}
+
+
+
